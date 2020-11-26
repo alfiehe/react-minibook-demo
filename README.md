3e13f3244f49cf7bc5b64c079c7945e6691290a6
alfiehe <alfiehe@tencent.com>
2020-11-26 16:15:55 +0800

    comments storage

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ npm start
 
 - 页面加载后`Content`输入框自动聚焦
 - 用户名LocalStorage
+
+---
+
+### 评论 `Storage`
+
+表单提交时缓存 Storage
+```js
+handleSubmitComment() {
+    localStorage.setItem('comments', JSON.stringify(this.state.comments));
+}
+```
+组件挂载时加载Storage
+```js
+componentWillMount() {
+    const comments = localStorage.getItem('comments');
+    comments && this.setState({ comments: JSON.parse(comments) });
+}
+```
--- src/CommentApp.js
+++ src/CommentApp.js
@@ -11,16 +11,25 @@ class CommentApp extends React.Component {
         }
     }
 
+    componentWillMount() {
+        const comments = localStorage.getItem('comments');
+        comments && this.setState({ comments: JSON.parse(comments) });
+    }
+
     // 接收 CommentInput 提交内容
     handleSubmitComment = (comment) => {
         // 校验是否填入内容
         if (!comment) { return };
         if (!comment.username) { return alert('请输入用户名') }
         if (!comment.content) { return alert('请输入评论内容') }
-        // 更新评论列表数据
+        const newComments = this.state.comments;
+        newComments.push(comment);
+        // 更新评论
         this.setState({
-            comments: [...this.state.comments, comment]
+            comments: newComments
         });
+        // 缓存评论
+        localStorage.setItem('comments', JSON.stringify(this.state.comments));
     }
 
     render() {
